83ba99a1ce2fd65633a64777955e41219cfcaf98
Laurent Brand <laurent@Laurents-MacBook-Pro.fritz.box>
2022-07-05 00:02:40 +0200

    editing homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="grid place-items-center space-y-16">
         <h1>Home</h1>
         <div class="w-1/2">
-          <h2 class="font-sans text-center">“Eng kulinaresch Entdeckungsrees duerch eise klenge Grand-Duché”</h2>
+          <h2 class="font-sans text-center truncate">“Eng kulinaresch Entdeckungsrees duerch eise klenge Grand-Duché”</h2>
         </div>
 
         <div class="w-2/5 border-b border-solid border-1 border-black"></div>
