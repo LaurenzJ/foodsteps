1dd78326d9e560a1ac1e5abf06bcea1aeace7ee2
Laurent Brand <laurent@Laurents-MacBook-Pro.fritz.box>
2022-07-08 00:33:45 +0200

    faq

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -34,6 +34,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/GalleryView.vue')
   },
   {
+    path: '/faq',
+    name: 'faq',
+    component: () => import(/* webpackChunkName: "about" */ '../views/FAQView.vue')
+  },
+  {
     path: '/newsletter',
     name: 'newsletter',
     component: () => import(/* webpackChunkName: "about" */ '../views/NewsletterView.vue')
---
+++ frontend/src/views/FAQView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="faw">
+    <div class="grid place-items-center space-y-16 px-6">
+        <h1>FAQ</h1>
+
+        <div class="text-center w-1/3 space-y-4">
+            <h2>Wéi kann ech bezuelen?</h2>
+            <p class="text-xl">Dir kënnt direkt iwwert des Websäit mat Kreditkaart oder ApplePay bezuelen. Dir kënnt eis awer och kontaktéieren an per virement bezuelen.</p>
+        </div>
+
+        <div class="text-center w-1/3 space-y-4">
+            <h2>Wéi kréien ech meng Dokumentatioun?</h2>
+            <p class="text-xl">Dir kënnt direkt iwwert des Websäit mat Kreditkaart oder ApplePay bezuelen. Dir kënnt eis awer och kontaktéieren an per virement bezuelen.</p>
+        </div>
+
+        <div class="text-center w-1/3 space-y-4">
+            <h2>Wéi kann ech bezuelen?</h2>
+            <p class="text-xl">Dir kënnt direkt iwwert des Websäit mat Kreditkaart oder ApplePay bezuelen. Dir kënnt eis awer och kontaktéieren an per virement bezuelen.</p>
+        </div>
+
+        <div class="text-center w-1/3 space-y-4">
+            <h2>Wéi kann ech bezuelen?</h2>
+            <p class="text-xl">Dir kënnt direkt iwwert des Websäit mat Kreditkaart oder ApplePay bezuelen. Dir kënnt eis awer och kontaktéieren an per virement bezuelen.</p>
+        </div>
+
+        <div class="text-center w-1/3 space-y-4">
+            <h2>Wéi kann ech bezuelen?</h2>
+            <p class="text-xl">Dir kënnt direkt iwwert des Websäit mat Kreditkaart oder ApplePay bezuelen. Dir kënnt eis awer och kontaktéieren an per virement bezuelen.</p>
+        </div>
+
+        <div class="text-center w-1/3 space-y-4">
+            <h2>Wéi kann ech bezuelen?</h2>
+            <p class="text-xl">Dir kënnt direkt iwwert des Websäit mat Kreditkaart oder ApplePay bezuelen. Dir kënnt eis awer och kontaktéieren an per virement bezuelen.</p>
+        </div>
+
+        <div class="text-center w-1/3 space-y-4">
+            <h2>Wéi kann ech bezuelen?</h2>
+            <p class="text-xl">Dir kënnt direkt iwwert des Websäit mat Kreditkaart oder ApplePay bezuelen. Dir kënnt eis awer och kontaktéieren an per virement bezuelen.</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FAQView',
+  
+}
+</script>
