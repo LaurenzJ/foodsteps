10df131a4406f207e29e5696b864eb7b12b0fede
Laurent Brand <laurent@Laurents-MacBook-Pro.local>
2022-07-07 18:16:21 +0200

    finished GalleryView view

--- frontend/src/views/GalleryView.vue
+++ frontend/src/views/GalleryView.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="gallery">
-    <div class="grid place-items-center">
+    <div class="grid place-items-center space-y-16 px-6">
         <h1>Gallery</h1>
+
+        <div class="md:w-2/3 w-full grid md:grid-cols-3 grid-cols-1 gap-4">
+          <img class="h-full" src="@/assets/images/gallery/1.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/2.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/3.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/4.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/5.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/6.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/7.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/8.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/9.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/10.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/11.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/12.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/13.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/14.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/15.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/16.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/17.png" alt="">
+          <img class="h-full" src="@/assets/images/gallery/18.png" alt="">
+        </div>
     </div>
   </div>
 </template>
